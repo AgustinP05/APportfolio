135445597987a49c5786c1384d5b6afc35b9ada7
Agustin <agustinperea5@gmail.com>
2024-03-17 20:22:40 -0300

    configurado astro para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(),tailwind()] 
+  integrations: [react(),tailwind()],
+  site: 'https://apportfolio.github.io',
+  base: '/APportfolio', 
 
 })
